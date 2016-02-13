f002831bd072353847f5e3f40d68e64c1b756cfb
Jay <jayk1994@gmail.com>
2016-02-13 11:33:54 -0800

    commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'yelp-app',
+	
+	template: `
+		<h1>Yelp App</h1>
+	`
+
+})
+
+export class AppComponent {
+	public title = 'Yelp App';
+}
