e2211e171bdd0b0baeea9db298a04697b28c0cd4
Keegan Caruso <keegan.caruso@email.wsu.edu>
2016-03-22 15:22:36 -0700

    Create UPDATE.sql
    added update

---
+++ UPDATE.sql
@@ -0,0 +1,8 @@
+UPDATE Businesses B
+set B.stars = (SELECT AVG(R.stars)
+             FROM Reviews R
+             WHERE R.business_id = B.business_id
+             GROUP BY B.business_id),
+    B.review_count = (SELECT COUNT(*)
+                      FROM Reviews R 
+                      Where R.business_id = B.business_id;
