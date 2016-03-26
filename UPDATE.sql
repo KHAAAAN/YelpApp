34ae09f20bcbeddd5d9c1c7b065e759c1bf57394
Keegan Caruso <keegan.caruso@email.wsu.edu>
2016-03-26 12:53:01 -0700

    Update UPDATE.sql

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,8 +1,12 @@
+USE MS2;
+
 UPDATE Businesses B
 set B.stars = (SELECT AVG(R.stars)
              FROM Reviews R
              WHERE R.business_id = B.business_id
-             GROUP BY B.business_id),
-    B.review_count = (SELECT COUNT(*)
+             GROUP BY B.business_id);
+             
+UPDATE Businesses B
+set B.review_count = (SELECT COUNT(*)
                       FROM Reviews R 
                       Where R.business_id = B.business_id;
